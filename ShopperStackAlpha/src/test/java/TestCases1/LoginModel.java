f8416c137be59d39b4aa5feaf79233bda5b81eb1
MYPC <MYPC@DESKTOP-1M8I9U9>
2023-09-08 16:44:29 +0530

    changed  1 testr case

--- ShopperStackAlpha/src/test/java/TestCases1/LoginModel.java
+++ ShopperStackAlpha/src/test/java/TestCases1/LoginModel.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import WebdriverUtility.BaseClass;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.webdriver.WebDriverBrowser;
 
@@ -23,11 +24,11 @@ public class LoginModel extends BaseClass{
 	@Test
 	public void ATc_Login_002() {
 		
-//		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
-//		WebDriver driver = new ChromeDriver();
-//		driver.get("https://www.makemytrip.com/");
-//		driver.quit();
-//		
+	System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+	driver.get("https://www.makemytrip.com/");
+		driver.quit();
+		
 		System.out.println("second test case");
 	}
 
